83a66c7ed3afb2982f18237e972f06bff5726594
pooja <poojagoyal5106@gmail.com>
2023-04-18 15:31:09 +0530

    final task 3

--- dom task 3/dom.js
+++ dom task 3/dom.js
@@ -19,20 +19,15 @@ console.log(headerTitle);
 headerTitle.textContent ="HELLO";
 headerTitle.innerText ="computer courses";
 headerTitle.innerHTML='<h1>computer courses</h1>';*/
-let title=document.getElementById("header-title");
 var header = document.getElementById("main-header");
-title.style.border="solid 4px #000";
 header.style.border="solid 4px #000";
 
 var items = document.getElementsByClassName("list-group-item");
 console.log(items);
 console.log(items[3]);
-items[2].style.color="red";
-items[2].style.background="yellow";
 
 
 var additems = document.getElementsByClassName("title");
 console.log(additems);
 additems[0].style.fontWeight="bold";
 additems[0].style.color="green";
-additems[0].style.fontStyle="italic";
