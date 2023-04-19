c153a242a38d84a3aad7772c1b0bcaeffd70b40f
pooja <poojagoyal5106@gmail.com>
2023-04-19 13:16:36 +0530

    task 5

---
+++ dom task 4/dom.js
@@ -0,0 +1,8 @@
+var li = document.getElementsByliName("list-group-item");
+console.log(li);
+console.log(li[1]);
+li[0].style.fontWeight = "bold"
+li[1].style.fontWeight = "bold"
+li[2].style.fontWeight = "bold"
+li[3].style.fontWeight = "bold"
+li[2].style.backgroundColor = "green"
