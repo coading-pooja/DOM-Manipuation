9211c9a141d5df6139f40d9238d3b049402ccf37
pooja <poojagoyal5106@gmail.com>
2023-04-19 11:36:44 +0530

    task 4

--- dom task 3/dom.js
+++ dom task 3/dom.js
@@ -19,15 +19,18 @@ console.log(headerTitle);
 headerTitle.textContent ="HELLO";
 headerTitle.innerText ="computer courses";
 headerTitle.innerHTML='<h1>computer courses</h1>';*/
-var header = document.getElementById("main-header");
-header.style.border="solid 4px #000";
+//var header = document.getElementById("main-header");
+//header.style.border="solid 4px #000";
+/*var additems = document.getElementsByClassName("title");
+console.log(additems);
+additems[0].style.fontWeight= "bold";
+additems[0].style.color="green";*/
 
 var items = document.getElementsByClassName("list-group-item");
 console.log(items);
-console.log(items[3]);
-
-
-var additems = document.getElementsByClassName("title");
-console.log(additems);
-additems[0].style.fontWeight="bold";
-additems[0].style.color="green";
+console.log(items[1]);
+items[0].style.fontWeight = "bold"
+items[1].style.fontWeight = "bold"
+items[2].style.fontWeight = "bold"
+items[3].style.fontWeight = "bold"
+items[2].style.backgroundColor = "green"
